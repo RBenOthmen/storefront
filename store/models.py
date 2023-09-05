8ef03f4f0ce05f1bf829cf3cef2d4c24568ff846
BenOthmenR <ramibenothmen15@gmail.com>
2023-09-05 11:45:10 +0100

    Defining Many-to-many Relationships

--- store/models.py
+++ store/models.py
@@ -4,6 +4,11 @@ from django.db import models
 # Create your models here.
 
 
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
 class Collection(models.Model):
     title = models.CharField(max_length=255)
 
@@ -15,6 +20,7 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
 
 
 class Customer(models.Model):
@@ -58,7 +64,6 @@ class Order(models.Model):
         street = models.CharField(max_length=255)
         city = models.CharField(max_length=255)
         customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-
 
     class OrderItem(models.Model):
         order = models.ForeignKey(Order, on_delete=models.PROTECT)
