63aff1f72d21e87705de7b9f8ab4adbc581fcf5f
BenOthmenR <ramibenothmen15@gmail.com>
2023-09-05 12:00:08 +0100

    Resolving Circular Relationships

--- store/models.py
+++ store/models.py
@@ -11,6 +11,9 @@ class Promotion(models.Model):
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey(
+        "Product", on_delete=models.SET_NULL, null=True, related_name="+"
+    )
 
 
 class Product(models.Model):
@@ -60,21 +63,21 @@ class Order(models.Model):
     )
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
+    class OrderItem(models.Model):
+        order = models.ForeignKey("Order", on_delete=models.PROTECT)
+        product = models.ForeignKey(Product, on_delete=models.PROTECT)
+        quantity = models.PositiveSmallIntegerField()
+        unitPrice = models.DecimalField(max_digits=6, decimal_places=2)
+
     class Adress(models.Model):
         street = models.CharField(max_length=255)
         city = models.CharField(max_length=255)
         customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
-    class OrderItem(models.Model):
-        order = models.ForeignKey(Order, on_delete=models.PROTECT)
-        product = models.ForeignKey(Product, on_delete=models.PROTECT)
-        quantity = models.PositiveSmallIntegerField()
-        unitPrice = models.DecimalField(max_digits=6, decimal_places=2)
-
     class Cart(models.Model):
         created_at = models.DateTimeField(auto_now_add=True)
 
     class CartItem(models.Model):
-        cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+        cart = models.ForeignKey("Cart", on_delete=models.CASCADE)
         product = models.ForeignKey(Product, on_delete=models.CASCADE)
         quantity = models.PositiveSmallIntegerField()
