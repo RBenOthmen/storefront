04dcdcf8aca7bcc2c3930cef30e723b34f31628d
BenOthmenR <ramibenothmen15@gmail.com>
2023-09-07 14:00:29 +0100

    Renamed unitPrice to unit_price in store

--- store/models.py
+++ store/models.py
@@ -68,7 +68,7 @@ class Order(models.Model):
         order = models.ForeignKey("Order", on_delete=models.PROTECT)
         product = models.ForeignKey(Product, on_delete=models.PROTECT)
         quantity = models.PositiveSmallIntegerField()
-        unitPrice = models.DecimalField(max_digits=6, decimal_places=2)
+        unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
     class Adress(models.Model):
         street = models.CharField(max_length=255)
