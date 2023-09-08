0f38cad9861b27282a0f61de6dbb3d71b7792169
BenOthmenR <ramibenothmen15@gmail.com>
2023-09-08 19:57:51 +0100

    Referencing Fields using F Objects

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q
+from django.db.models import Q, F
 from store.models import Product
 
 
 # Create your views here.
 def say_hello(request):
-    query_set = Product.objects.filter(Q(inventory__lt=10) & ~Q(unit_price__lt=20))
+    query_set = Product.objects.filter(inventory=F("collection__id"))
     return render(request, "hello.html", {"name": "Rami", "products": list(query_set)})
