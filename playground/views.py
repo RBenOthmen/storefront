a9093e94da030e925129d89ecf047ed223478792
BenOthmenR <ramibenothmen15@gmail.com>
2023-09-08 22:48:01 +0100

    Fixed inner class bug & renaming Adress to Address

--- playground/views.py
+++ playground/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F
-from store.models import Product
+from store.models import Product, OrderItem
 
 
 # Create your views here.
 def say_hello(request):
-    query_set = Product.objects.order_by("unit_price")[5:10]
+    query_set = OrderItem.objects.order_by("unit_price")[5:10]
     return render(request, "hello.html", {"name": "Rami", "products": list(query_set)})
--- store/models.py
+++ store/models.py
@@ -64,21 +64,25 @@ class Order(models.Model):
     )
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
-    class OrderItem(models.Model):
-        order = models.ForeignKey("Order", on_delete=models.PROTECT)
-        product = models.ForeignKey(Product, on_delete=models.PROTECT)
-        quantity = models.PositiveSmallIntegerField()
-        unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
-    class Adress(models.Model):
-        street = models.CharField(max_length=255)
-        city = models.CharField(max_length=255)
-        customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
-    class Cart(models.Model):
-        created_at = models.DateTimeField(auto_now_add=True)
 
-    class CartItem(models.Model):
-        cart = models.ForeignKey("Cart", on_delete=models.CASCADE)
-        product = models.ForeignKey(Product, on_delete=models.CASCADE)
-        quantity = models.PositiveSmallIntegerField()
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
