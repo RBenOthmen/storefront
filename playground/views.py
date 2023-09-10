52f0091ee8e7aa899f39fdd1663df6e9cc222f55
BenOthmenR <ramibenothmen15@gmail.com>
2023-09-10 20:24:53 +0100

    Transactions

--- playground/views.py
+++ playground/views.py
@@ -2,17 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F, Value, Func, Count, ExpressionWrapper
+from django.db import transaction
 from django.db.models.functions import Concat
 from django.db.models.fields import DecimalField
 from tags.models import TaggedItem, ContentType
-from store.models import Product, Collection
+from store.models import Product, Collection, Order, OrderItem
 
 
 # Create your views here.
 def say_hello(request):
-    collection = Collection(pk=11)
-    collection.delete()
+    with transaction.atomic():
+        order = Order()
+        order.customer_id = 9
+        order.save()
 
-    Collection.objects.filter(id__lt=5).delete()
+        item = OrderItem()
+        item.order = order
+        item.product_id = 1
+        item.quantity = 1
+        item.unit_price = 10
+        item.save()
 
     return render(request, "hello.html", {"name": "Rami"})
