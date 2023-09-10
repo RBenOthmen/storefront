9469be06772bd0cd1d995ba0ed88c2d2a0ccce1d
BenOthmenR <ramibenothmen15@gmail.com>
2023-09-10 20:39:43 +0100

    Executing Raw SQL Queries

--- playground/views.py
+++ playground/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F, Value, Func, Count, ExpressionWrapper
-from django.db import transaction
+from django.db import transaction, connection
 from django.db.models.functions import Concat
 from django.db.models.fields import DecimalField
 from tags.models import TaggedItem, ContentType
@@ -11,16 +11,8 @@ from store.models import Product, Collection, Order, OrderItem
 
 # Create your views here.
 def say_hello(request):
-    with transaction.atomic():
-        order = Order()
-        order.customer_id = 9
-        order.save()
-
-        item = OrderItem()
-        item.order = order
-        item.product_id = 1
-        item.quantity = 1
-        item.unit_price = 10
-        item.save()
+    # with connection.cursor() as cursor:
+    #     cursor.callproc('get_customers', [1, 2, 'a'])
+    query_set = Product.objects.raw('SELECT id, title FROM store_product')
 
     return render(request, "hello.html", {"name": "Rami"})
