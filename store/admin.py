e3fe9ed1d48ac97da9189d7891cb850ca1d793eb
BenOthmenR <ramibenothmen15@gmail.com>
2023-09-12 11:54:48 +0100

    Adding Search to the List Page

--- store/admin.py
+++ store/admin.py
@@ -2,8 +2,10 @@ import django.db.models
 from typing import Any
 from django.contrib import admin
 from django.db.models.query import QuerySet
-from django.db.models import Count
+from django.db.models.aggregates import Count
 from django.http.request import HttpRequest
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
 from . import models
 
 
@@ -36,11 +38,18 @@ class OrderAdmin(admin.ModelAdmin):
 
 # @admin.register(models.Collection)
 # class CollectionAdmin(admin.ModelAdmin):
-#     list_display = ("title")
-#     # list_per_page = 10
+#     list_display = ["title", "products_count"]
 
+#     @admin.display(ordering="products_count")
 #     def products_count(self, collection):
-#         return collection.products_count
+#         url = (
+#             reverse("admin:store_product_changelist")
+#             + "?"
+#             + urlencode({"collection__id": str(collection.id)})
+#         )
+#         return format_html(
+#             '<a href="http://google.com">{}</a>', collection.products_count
+#         )
 
 #     def get_queryset(self, request):
 #         return super().get_queryset(request).annotate(products_count=Count("product"))
@@ -50,8 +59,9 @@ class OrderAdmin(admin.ModelAdmin):
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ["first_name", "last_name", "membership"]
     list_editable = ["membership"]
-    ordering = ["first_name", "last_name"]
     list_per_page = 10
+    ordering = ["first_name", "last_name"]
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
 
 admin.site.register(models.Collection)
