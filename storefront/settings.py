8f9b380a7dc52c0c75f26638df59746e8906007a
BenOthmenR <ramibenothmen15@gmail.com>
2023-09-12 21:03:54 +0100

    Extending Pluggable Apps

--- storefront/settings.py
+++ storefront/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "playground",
     "debug_toolbar",
     "store",
+    "store_custom",
     "tags",
     "likes",
 ]
@@ -97,7 +98,7 @@ DATABASES = {
 
 
 # Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators 
+# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
